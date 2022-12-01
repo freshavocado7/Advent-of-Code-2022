70c8454f153539eb0a6faaf2e403cd7a6efcfd6d
freshavocado7 <48321780+freshavocado7@users.noreply.github.com>
2022-12-01 15:04:18 +0200

    Update adventDay01.py

--- adventDay01.py
+++ adventDay01.py
@@ -1,30 +1,34 @@
-import os
+#Initialize variables
+sum = 0                  
+solution2=0
+caloryElfs = []
+readList = []
 
-path = "/home/nick/Desktop/AoD/adventDay01"
-os.chdir(path)
-
-
-lst = []
+#Read file and append to list "readList"
 with open('elfCalories.txt') as my_file:
-     lst = my_file.readlines()
+     readList = my_file.readlines()
 
-elfs = [item.replace('\r', '').replace('\n', '') for item in lst]
-print(elfs)
+#Clean up readList and append to elfs
+elfs = [item.replace('\n', '') for item in readList]
 
-sum = 0
-max = -1
-
-
-
-
+#Create new list caloryElfs with the sums of Calories from each elf
 for i in range(len(elfs)):
     if elfs[i] != '':
         sum = sum + int(elfs[i])
-        if sum > max:
-            max = sum
     else:
+        caloryElfs.append(sum)
         sum = 0
 
-print (max)    
+#Sort list caloryElfs from high to low
+caloryElfs.sort(reverse=True)
+
+#Sum up the top 3 elfs from sorted caloryElf list. This is for the second part of the challenge
+for i in range (0,3):
+    solution2 = solution2 + caloryElfs[i]
+
+#First index of sorted caloryElfs list is our solution for the first part of the challenge
+solution1 = caloryElfs[0]
+print("Highest calory elf:", solution1)
+print("Sum of top 3 calory elfs:", solution2)
 
 
